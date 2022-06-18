cb49c22758145a246415d460422d7b8ee7c3fc05
Jessica Card <jessicard@mac.com>
2022-06-18 15:39:02 -0400

    Start with nil

--- day1/script.rb
+++ day1/script.rb
@@ -1,9 +1,9 @@
-previous_line = "130"
+previous_line = nil
 increases = 0
 
 File.open("input.txt", "r") do |f|
   f.each_line do |line|
-    if previous_line.to_i < line.to_i
+    if previous_line && previous_line.to_i < line.to_i
       increases = increases + 1
     end
 
